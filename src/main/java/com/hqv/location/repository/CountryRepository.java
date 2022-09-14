032cbb762840861097f945d599c6eb31c035a11b
Mohammed Qurram <mdqurramhhh@gmail.com>
2022-09-14 09:54:00 -0400

    qurrams 1st commit

---
+++ src/main/java/com/hqv/location/repository/CountryRepository.java
@@ -0,0 +1,12 @@
+package com.hqv.location.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hqv.location.entity.Country;
+
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long>{
+
+}
